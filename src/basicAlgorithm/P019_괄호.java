41bb6718f953e3c02d0d313c2fb57a37957f29ef
JUNGJUNILL <dala1207@naver.com>
2021-11-03 18:01:36 +0900

    commit_

--- src/basicAlgorithm/P019_괄호.java
+++ src/basicAlgorithm/P019_괄호.java
@@ -1,10 +1,40 @@
 package basicAlgorithm;
 
+import java.util.Stack;
+
 public class P019_괄호 {
 
+	//https://www.acmicpc.net/problem/9012
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//stack은 후입선출이다. 
+		
+		String str = "()"; 
 
+		Stack<Character> stack =new Stack<>(); 
+		StringBuffer bf=new StringBuffer(); 
+		
+		for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)=='('){
+				stack.push(str.charAt(i)); 
+			
+			}else{
+				
+					if(str.charAt(i)==')' && stack.pop()=='('){
+						
+					}else{
+						
+					}
+					
+					if(stack.isEmpty()){bf.append("NO\n"); break;}
+			}
+		
+		}
+		
+		System.out.println(bf.toString());
+		
+		
+		
 	}
 
 }
