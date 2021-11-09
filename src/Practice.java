401ee4469617b8e9aca56608fa037c89e3267379
dala1207@naver.com <dala1207@naver.com>
2021-11-10 07:18:39 +0900

    commit_20211110

--- src/Practice.java
+++ src/Practice.java
@@ -1,79 +1,96 @@
+import java.awt.print.Book;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class Tree {
-	class Node{
-		int data; 
-		Node left; 
-		Node right; 
-		public Node(int data){
-			this.data = data; 
-			
-		}
-		
+import javax.print.DocFlavor.CHAR_ARRAY;
+
+class GraphNode{
+	
+	int vertex; 
+	GraphNode link; 
+	
+}
+
+class AdjList{
+	GraphNode[] head = new GraphNode[10]; 
+	private int totalV=0; 
+	
+	
+	public void insertVertex(int v){
+		totalV++; 
 	}
 	
-	Node root; 
-	
-	
-	public Node makeTreeR(int[] arr, int start, int end){
+	public void insertEdge(int v1, int v2){
 		
-		if(start>end)return null; 
-		int mid = (start+end) / 2; 
-		Node node = new Node(arr[mid]); 
-		node.left = makeTreeR(arr, start, mid-1); 
-		node.right = makeTreeR(arr, mid+1, end); 
-		return node; 
-		
-	}
-	
-	public void makeTree(int[] arr){
-		root = makeTreeR(arr, 0, arr.length-1); 
-	}
-	
-	public Node searchTree(Node n ,int find){
-		
-		if(n==null){
-			return null; 
-		}
-		
-		Node node =n; 
-		Node nodeLeft = n.left; 
-		Node nodeRight = n.right; 
-		
-		
-		if(find > node.data){
-			
-			return searchTree(nodeRight, find);
-			
-		}else if(find < node.data){
-			return searchTree(nodeLeft, find); 
+		if(v1 >= totalV || v2 >=totalV){
+			System.out.println("그래프에 없는 정점입니다.");
 		}else{
-			return node; 
+			GraphNode gNode = new GraphNode(); 
+			gNode.vertex = v2; 
+			gNode.link = head[v1]; 
+			head[v1] = gNode;
+		}
+	}
+	
+	public void BFS(int v){
+		GraphNode w = new GraphNode(); 
+		Queue<Integer> q = new LinkedList<Integer>(); 
+		boolean[] visited =new boolean[10]; 
+		visited[v] = true; 
+		System.out.printf(" %c",v+65);
+		q.add(v); 
+		
+		while(!q.isEmpty()){
+			v = q.poll(); 
+			
+			for(w=head[v]; w !=null; w=w.link){
+				if(visited[w.vertex] == false){
+					visited[w.vertex] = true; 
+					System.out.printf(" %c",w.vertex+65);
+					q.add(w.vertex);
+				}
+			}
+			
+			
+			
 		}
 		
-
+		
+		
+		
 	}
 	
 	
 }
 
 
-
-
 public class Practice {
 	
 	public static void main(String[] args) throws Exception{
 
-		int[] arr ={1,2,3,4,5}; 
-		
-		Tree t =new Tree(); 
-		
-		t.makeTree(arr);
-		if(t.searchTree(t.root, 89)==null){
-			System.out.println("없음");
-		}else{
-			System.out.println("있음");
-		}
+		AdjList G9 =new AdjList(); 		
+		for(int i=0; i<7; i++){
 			
+			G9.insertVertex(i);
+		}
+		G9.insertEdge(0, 2);
+		G9.insertEdge(0, 1);
+		G9.insertEdge(1, 4);
+		G9.insertEdge(1, 3);
+		G9.insertEdge(1, 0);
+		G9.insertEdge(2, 4);
+		G9.insertEdge(2, 0);
+		G9.insertEdge(3, 6);
+		G9.insertEdge(3, 1);
+		G9.insertEdge(4, 6);
+		G9.insertEdge(4, 2);
+		G9.insertEdge(4, 1);
+		G9.insertEdge(5, 6);
+		G9.insertEdge(6, 5);
+		G9.insertEdge(6, 4);
+		G9.insertEdge(6, 3);
+		
+		G9.BFS(0);
 		
 	}
 }
---
+++ src/codeUpbasic100/P040_1042정수2개입력받아나눈몫출력하기.java
@@ -0,0 +1,17 @@
+package codeUpbasic100;
+
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.concurrent.SynchronousQueue;
+
+import org.omg.CORBA.INTERNAL;
+
+public class P040_1042정수2개입력받아나눈몫출력하기 {
+
+	public static void main(String[] args) {
+
+
+
+		
+	}
+}
