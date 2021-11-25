feb22dc425df38e0ef01183064e829b8da0cbb1d
JUNGJUNILL <dala1207@naver.com>
2021-11-25 18:02:07 +0900

    commit_company

--- src/Practice.java
+++ src/Practice.java
@@ -5,26 +5,126 @@ import java.util.*;
 public class Practice {
 
 	
+	 /*
+	   
+	   {
+			{1,1,0},
+			{1,1,0},
+			{0,0,1} 
+	   }
+	  
+		0컴 1컴 연결
+			1컵 1컴 연결
+			2컵 2컴 연결 
+		
+		
+		(0,0)=1 (0,1)=1 (0,2)=0
+		(1,0)=1 (1,1)=1 (1,2)=0
+		(2,0)=0 (2,1)=0 (2,2)=1
+		
+		
+		{
+		 	{1, 1, 0}, 
+		 	{1, 1, 1}, 
+		 	{0, 1, 1}
+		 }
+		 
+		 0컴 1컴 연결
+		 1컴 2컴 연결 
+		 
+		(0,0)=1 (0,1)=1 (0,2)=0
+		(1,0)=1 (1,1)=1 (1,2)=1
+		(2,0)=0 (2,1)=1 (2,2)=1
+		 
+		
+	  */
+	
+	static int[][] array =	{
+		 	{1, 1, 0}, 
+		 	{1, 1, 1}, 
+		 	{0, 1, 1}
+		 
+		 }; 
+	static int n=3; 
+	static int[][] D ={
+			{-1,0},  //위로 한칸(상)
+			{1,0},   //아래로 한칸(하)
+			{0,-1},  //왼쪽 한칸 (좌)
+			{0,1},   //오른쪽 한칸(우)  를 하는 로직이다.
+	};
+	static boolean[][] visited = new boolean[n][n]; 
+	static int[][] newArray; 
+	
+	static int solution(int n, int[][] computer){
+		
+		int answer=0; 
+
+		
+		for(int i=0; i<computer.length; i++){
+			for(int j=0; j<computer[i].length; j++){
+				if(computer[i][j]==1 && !visited[i][j]){
+					//System.out.println("BFS("+i+","+j+")");
+					Stack<Integer> stack =new Stack<>(); 
+					stack.push(computer[i][j]); 
+					 
+					
+					while(!stack.isEmpty()){
+						int curr = stack.pop(); 
+						visited[i][j]=true;
+			
+						
+						
+						for(int k=0; k<4; k++){
+							int nr = i +D[k][0]; 
+							int nc = j +D[k][1]; 
+
+							if(nr<0 || nr>n-1 || nc<0 || nc>n-1)continue; 
+							if(visited[nr][nc])continue; 
+							if(computer[nr][nc]==0)continue; 
+							visited[nr][nc]=true; 
+							stack.push(computer[nr][nc]); 
+							
+						}//end of for 
+
+					}//end of while
+				
+					answer++;
+				}	
+			}
+		}
+		
+
+		return answer; 
+		
+		
+	
+		
+	
+	}
+	
 
 	//https://programmers.co.kr/learn/courses/30/lessons/43162
 	public static void main(String[] args) throws Exception{
 	
-		int[][] array = {
-				{1,1,0},
-				{1,1,0},
-				{0,0,1} }; 
-	/*
-	0컴 1컴 연결
-	1컵 1컴 연결
-	2컵 2컴 연결 
+		for(int i=0; i<array.length; i++){
+			for(int j=0; j<array[i].length; j++){
+				System.out.print(array[i][j]+" ");
+			}
+			System.out.println();
+
+		}
 	
-	(0,0)=1 (0,1)=1 (0,2)=0
-	(1,0)=1 (1,1)=1 (1,2)=0
-	(2,0)=0 (2,1)=0 (2,2)=1
-	 
-	 
-	  */
-	
+		System.out.println();
+		System.out.println(solution(3,array));
+		System.out.println();
+		for(int i=0; i<visited.length; i++){
+			for(int j=0; j<visited[i].length; j++){
+				System.out.print(visited[i][j]+" ");
+			}
+			System.out.println();
+
+		}
+		
 	}
 }
 
--- src/codeUpbasic100/P062_1065정수3개입력받아짝수만출력하기.java
+++ src/codeUpbasic100/P062_1065정수3개입력받아짝수만출력하기.java
@@ -12,6 +12,17 @@ public class P062_1065정수3개입력받아짝수만출력하기 {
 	public static void main(String[] args) {
 
 		
+		Scanner sc =new Scanner(System.in); 
+		
+		int a = sc.nextInt(); 
+		int b= sc.nextInt(); 
+		int c= sc.nextInt(); 
+		
+		System.out.print(a %2==0 ? a+" " : " ");
+		System.out.print(b %2==0 ? b+" " : " ");
+		System.out.print(c %2==0 ? c+" " : " ");
+		
+		
 		
 	}
 }
