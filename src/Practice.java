7a11e269fc7abd302e791f88fc6cd2a467003570
JUNGJUNILL <dala1207@naver.com>
2021-12-15 17:58:35 +0900

    commit_company

--- src/Practice.java
+++ src/Practice.java
@@ -1,37 +1,130 @@
 import java.beans.Visibility;
 import java.util.*;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 
 public class Practice {
 
-	//https://www.acmicpc.net/source/share/9e95120c2a744876813ae5198c84993b
+	static boolean[] visited=new boolean[5]; 
+	static ArrayList<Integer>[] list; 
+	public static void DFS(int x, int len){
+		if(len==4){
+			System.out.println(1);
+			return; 
+		}
+		visited[x]=true;
+		for(int i=0; i<list[x].size(); i++){
+			int temp= list[x].get(i); 
+			if(!visited[temp]){
+				visited[temp]=true;
+				DFS(temp,len+1); 
+				visited[temp]=false; 
+				
+			}
+		}
+		
+		/*
+		 DFS(0,0) 
+		 [0] = {1} 
+		 FOR 0 list[0].size()==1 
+		 temp list[0].get(0)==1 
+		 !visited[1] true
+		 visited[1]=true
+		 		DFS(1,1)
+		 		[1] = {0,2}
+		 		FOR 0 continue 
+		 			   1 list[1].size()==2
+		 			   temp = list[1].get(1)==2
+		 			   !visited[2] true 
+		 			   visited[2]=true 
+		 			   			DFS(2,2) 
+		 			   			[2] = {1,3}
+		 			   			for 0 list[2].get(0)==1 continue 
+		 			   				 1 list[2].get(1)==3 
+		 			   				 temp = list[2].get(1)==3 
+		 			   				 !visited[3] true 
+		 			   				 visited[3]=true
+		 			   				 		  DFS(3,3)
+		 			   				 		  [3] = {2,4} 
+		 			   				 		  FOR 0 list[3].get(0)==2 continue 
+		 			   				 		         1 list[3].get(1)==4
+		 			   				 		         temp list[3].get(1)==4 
+		 			   				 		         !visited[4] true 
+		 			   				 		         visited[4]=true 
+		 			   				 		         		  DFS(4,4)--------------´ä
+		 			   				 		         		  [4] = {3} 
+		 			   				 		         		  FOR 0 list[4].get(0)==3 continue
+		 			   				 		         		  
+		 			   				 		         visited[4]=false 
+		 			   				 visied[3]=false 
+		 			   visited[2]=false
+		 visited[1]=false; 
+		 
+		  DFS(1,0)
+		     
+		     
+		  DFS(2,0)
+		  
+		  
+		  DFS(3,0)
+		  
+   
+		  DFS(4,0) 
+		  
+		  
+		 */
+		
+		
+		
+	}
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in); 
 		
-		int N=5; 
-		int M=5; 		
-		int[][] graph=new int[N][N]; 
-		
-		for(int i=0; i<M; i++){
-			int row=sc.nextInt(); 
-			int col=sc.nextInt(); 
-			graph[row][col]=graph[col][row]=1; 
-
+		list =new ArrayList[5]; 
+		for(int i=0; i<5; i++){
+			list[i]=new ArrayList<>(); 
 		}
+		list[0].add(1); 
+		list[1].add(0); 
 		
-		for(int i=0; i<graph.length; i++){
-			for(int j=0; j<graph[i].length; j++){
-				System.out.print(graph[i][j]+" ");
-			}
-			System.out.println();
-		}
+		list[1].add(2); 
+		list[2].add(1); 
 		
-		for(int i=0;i<graph.length; i++){
-			
-		}
+		list[2].add(3); 
+		list[3].add(2);
+		
+		list[3].add(4); 
+		list[4].add(3); 
+//		System.out.println("size="+list[3].size());
+//		for(int j=0; j<5; j++){
+//			visited=new boolean[5]; 
+//		
+//		}
+		 		
+		DFS(0,0);
+		DFS(1,0);
+		DFS(2,0); 
+		DFS(3,0); 
+		DFS(4,0);
+		ArrayList<Integer> list0 =new ArrayList<>(); //[1]
+		ArrayList<Integer> list1 =new ArrayList<>(); //[0,2]
+		ArrayList<Integer> list2 =new ArrayList<>(); //[1,3]
+		ArrayList<Integer> list3 =new ArrayList<>();//[2,4]
+		ArrayList<Integer> list4 =new ArrayList<>();//[3]
 		
 		
+		list0.add(1); 
+		list1.add(0); 
+		
+		list1.add(2); 
+		list2.add(1); 
+		
+		list2.add(3); 
+		list3.add(2);
+		
+		list3.add(4); 
+		list4.add(3); 
 	}
 }
 
--- src/백준_알고리즘_기초02/그래프1_ABCDE.java
+++ src/백준_알고리즘_기초02/그래프1_ABCDE.java
@@ -1,14 +1,82 @@
 package 백준_알고리즘_기초02;
 
+import java.util.ArrayList;
+
 public class 그래프1_ABCDE {
 
+	//https://www.acmicpc.net/problem/13023
 	//https://www.acmicpc.net/source/share/9e95120c2a744876813ae5198c84993b
+	//https://moonsbeen.tistory.com/161
 	//문제를 제대로 이해하지 못한 상태에서 문제를 풀었다. 
 	//그리고 설레발을 너무 심하게 쳤다. 
+	//0-[1]
+	//1-[0,2]
+	//2-[1,3]
+	//3-[2,4]
+	//4-[3]
+	
+	
+	//ArrayList[0]-[1]   -size:1
+	//ArrayList[1]-[0,2] -size:2
+	//ArrayList[2]-[1,3] -size:2
+	//ArrayList[3]-[2,4] -size:2
+	//ArrayList[4]-[3]   -size:1
+	/*
+    0 1
+	1 2
+	2 3
+	3 4
+	 */
+	static int n; 
+	static ArrayList<Integer>[] list; 
+	static boolean[] visited=new boolean[5]; 
+	public static void DFS(int x, int len){
+
+		if(len==4){
+		
+			System.out.println(1);
+		}
+		visited[x]=true; 
+		for(int i=0; i<list[x].size(); i++){
+			int temp = list[x].get(i); 
+			if(!visited[temp]){
+				visited[temp]=true;
+				DFS(temp,len+1); 
+				visited[temp]=false; 
+			}
+		}
+		
+	}
+
+	
 	public static void main(String[] args) {
+		
+		int n=5; 
+		int m=5; 
+		list = new ArrayList[n]; 
+		for(int i=0; i<n; i++){
+			list[i]=new ArrayList<>(); 
+		}
+
+		//솔직히 이 로직이 진짜 이해 안된다.
+		list[0].add(1); 
+		list[1].add(0); 
+		
+		list[1].add(2); 
+		list[2].add(1);
+		
+		list[2].add(3); 
+		list[3].add(2);
+		
+		list[3].add(4); 
+		list[4].add(3); 
+		
+		for(int i=0; i<n; i++){
+			
+			DFS(i,0);
+		}
 
 		
 		
 	}
-
 }
