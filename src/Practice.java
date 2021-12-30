751622112191d6e7d1c1c931868a056276d01911
dala1207@naver.com <dala1207@naver.com>
2021-12-30 23:44:35 +0900

    commit_20211230

--- src/Practice.java
+++ src/Practice.java
@@ -5,11 +5,73 @@ public class Practice {
 
 
 	public static void main(String[] args) {
-		for(int i=0; i<=4; i++){
-			System.out.println(i);
+		int n=4;
+		
+		ArrayList<Integer>[] a = new ArrayList[n]; 
+		int[] parent = new int[n]; 
+		int[] order  = {0,1,2,3}; // 그래프 순회 결과 
+		boolean[] check=new boolean[n]; 
+		
+		for(int i=0; i<n; i++){
+			a[i]=new ArrayList<>(); 
 		}
-		 
+		//그래프를 인접리스트로 표현한 모양이다.
+		a[0].add(1);
+		a[0].add(2);
+		
+		a[1].add(0);
+		a[1].add(3);
+		
+		a[2].add(0); 
+		
+		a[3].add(1); 
+		
+		Queue<Integer> q=new LinkedList<>(); 
+		q.add(0); 
+		check[0]=true; 
+		
+		int m=1; 
+		for(int i=0; i<n; i++){
+			
+			if(q.isEmpty()){
+				System.err.println(0);
+				System.exit(0);
+			}
+			
+			int x=q.poll(); 
+			if(x!=order[i]){
+				System.out.println(0);
+				System.exit(0);
+			}
+			
+			
+			int cnt=0; 
+			for(int y : a[x]){
+				if(!check[y]){
+					parent[y]=x;
+					cnt+=1;
+				}
+			}
+			
+			for(int j=0; j<cnt; j++){
+				
+				if((m+j) >=4 || parent[order[m+j]] !=x){
+					System.out.println(0);
+					System.exit(0);
+				}
+			
+				q.add(order[m+j]);
+				check[order[m+j]]=true; 
+			}
+			
+			m+=cnt; 
 		}
+		System.out.println(1);
+
+		
+		
+		
+	}
 }
 
 
--- src/백준_알고리즘_기초02/P022_그래프1_BFS스페셜저지.java
+++ src/백준_알고리즘_기초02/P022_그래프1_BFS스페셜저지.java
@@ -4,81 +4,75 @@ import java.util.*;
 
 public class P022_그래프1_BFS스페셜저지 {
 	
-	static boolean[] visited;
+
 	
-	static void BFS(int start){
-		
-	}
 
 	
 	//정답
 	//https://www.acmicpc.net/source/share/6c58c942cff74fc39d27f890916cce95
 	//https://www.acmicpc.net/problem/16940
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
-		int n=sc.nextInt(); 
-		ArrayList<Integer>[] a = new ArrayList[n]; 
-		
-		int[] parent = new int[n]; 
-		int[] order =new int[n]; 
-		visited = new boolean[n]; 
-		
-		for(int i=0; i<n; i++){
-			a[i]=new ArrayList<>(); 
-		}
-		
-	
-		for(int i=0; i<n-1; i++){
-			int u=sc.nextInt()-1; 
-			int v=sc.nextInt()-1; 
-			
-			a[u].add(v); 
-			a[v].add(u); 
-		}
-		
-		for(int i=0; i<n; i++){
-			order[i]=sc.nextInt()-1; 
-		}
-		//[0,1,2,3]
-		
-		Queue<Integer> q = new LinkedList<Integer>(); 
-		q.add(0); 
-		visited[0]=true; 
-		int m=1; 
-		
-		for(int i=0; i<n; i++){
-			if(q.isEmpty()){
-				System.out.println(0);
-				System.exit(0);
-			}
-			
-			int x = q.poll(); 
-			if(x !=order[i]){
-				System.out.println(0);
-				System.exit(0);
-			}
-			
-			int cnt=0;
-			for(int y : a[x]){
-				if(!visited[y]){
-					parent[y]=x;
-					cnt+=1; 
-				}
-			}
-			
-			for(int j=0; i<cnt; j++){
-				if(m+j >=n || parent[order[m+j]] !=x){
-					System.out.println(0);
-					System.exit(0);
-				}
-				q.add(order[m+j]); 
-				visited[order[m+j]]=true; 
-				
-			}
-			m+=cnt; 
+		  Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        ArrayList<Integer>[] a = new ArrayList[n];
+	        int[] parent = new int[n];
+	        int[] order = new int[n];
+	        boolean[] check = new boolean[n];
+	        for (int i=0; i<n; i++) {
+	            a[i] = new ArrayList<>();
+	        }
+	        for (int i=0; i<n-1; i++) {
+	            int u = sc.nextInt()-1;
+	            int v = sc.nextInt()-1;
+	            a[u].add(v);
+	            a[v].add(u);
+	        }
+	        for (int i=0; i<n; i++) {
+	            order[i] = sc.nextInt()-1;
+	        }
+	        Queue<Integer> q = new LinkedList<>();
+	        q.add(0);
+	        check[0] = true;
+	        
+	        int m = 1;
+	        for (int i=0; i<n; i++) {
+	        
+	        	
+	        	if (q.isEmpty()) {
+	                System.out.println(0);
+	                System.exit(0);
+	            }
+	            int x = q.remove();
 
-		}
-		System.out.println(1);
+	            //첫번째 순회결과부터 다를 경우 
+	            if (x != order[i]) {
+	                System.out.println(0);
+	                System.exit(0);
+	            }
+	            
+	            int cnt = 0;
+	            for (int y : a[x]) {
+
+	                if (check[y] == false) {
+	                    parent[y] = x;
+	                    cnt += 1;
+	                }
+	            }
+	            for (int j=0; j<cnt; j++) {
+	            	
+	                if (m+j >= n || parent[order[m+j]] != x) {
+	                	System.out.println("이건 언제 실행되는거냐?");
+	                    System.out.println(0);
+	                    System.exit(0);
+	                } 
+	
+	                q.add(order[m+j]);
+	                check[order[m+j]] = true;
+	            }
+	            m += cnt;
+	        }
+	        System.out.println("m="+4);
+	        System.out.println(1);
 		
 	}
 }
