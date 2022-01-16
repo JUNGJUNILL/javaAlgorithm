41fa9f3b8d7f85289dab06a33a74cc29c467ebfd
dala1207@naver.com <dala1207@naver.com>
2022-01-16 22:27:47 +0900

    commit_20220116

--- src/Practice.java
+++ src/Practice.java
@@ -1,16 +1,29 @@
 
-import java.util.*; 
+import java.util.*;
+import java.util.concurrent.SynchronousQueue; 
 
 
 public class Practice {
-	
 
-	
 	public static void main(String[] args) {
 
-		for(int i=0; i<10; i++){
-			if(i==1) continue; 
-			System.out.println(i);
+		String str05 = "영영영일일일이이이삼사오"; 
+		Queue<Character> q=new LinkedList<Character>(); 
+		
+		
+		for(int i=0; i<str05.length(); i++){
+			
+			if(!q.contains(str05.charAt(i))){
+				q.add(str05.charAt(i));
+			}
+			
 		}
+		System.out.println(q.size());
+		
+		while(!q.isEmpty()){
+			System.out.print(q.poll());
+		}
+		
+		
 	}
 }
