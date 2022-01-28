177c7e02f553e1dde48eb367728f0c690e62fb19
JUNGJUNILL <dala1207@naver.com>
2022-01-28 14:01:48 +0900

    commit_20220128

--- src/Practice.java
+++ src/Practice.java
@@ -4,42 +4,8 @@ import java.util.*;
 
 public class Practice {
 
-	//https://programmers.co.kr/learn/courses/30/lessons/12933
 	public static void main(String[] args) {
-		
-		String str=""; 
-		
-		str="정"+str; //정""
-		str="준"+str; //정""+"준"+정""
-		//준정 
-		System.out.println(str);
 
-		String answer=""; 
-		int n=10; 
-		String[] number={"4","1","2"}; 
-		
-		while(n>0){
-			answer = number[n % 3] + answer; 
-			//answer = number[1] + answer; 
-			//answer = "1"+""; 
-			//answer = "1"
-			
-			//answer = number[0] + answer; 
-			//answer = "4"+"1"+""
-			//answer = 41 
-			
-			
-			n= (n-1) / 3; 	
-			//n = 9 / 3
-			//n=3 
-			
-			//n = 2 / 3; 
-			//n = 0 
-			//end of while
-			
-			
-		}
-		
 		
 		
 	}
---
+++ src/programmers_level02/P012_땅따먹기_DP.java
@@ -0,0 +1,81 @@
+package programmers_level02;
+
+import java.util.ArrayList;
+
+public class P012_땅따먹기_DP {
+
+//   static int solution(int[][] land) {
+//        int answer = 0;
+//
+//        System.out.println("Hello Java");
+//
+//        return answer;
+//    }
+
+	
+	static int[][] graph={
+					{1,2,3,5},
+					{5,6,7,8},
+					{4,3,2,1}
+					};
+	
+	
+	//피보나치 수열
+	//1 1 2 3 5 8 13 21 34 55 
+	static int[] newArr=new int[100]; 
+	static int DP(int x){
+		
+		if(x <=2) return 1; 
+		if(newArr[x]!=0) return newArr[x]; 
+		return newArr[x]=DP(x-1)+DP(x-2); 
+		
+	}
+	
+	//내가 애초에 풀 수 없었던 문제이다.
+	//너무 고집 부리지 말자.
+	//https://programmers.co.kr/learn/courses/30/lessons/12913
+	
+	//https://fbtmdwhd33.tistory.com/241
+	public static void main(String[] args) {
+		
+		int answer=Integer.MIN_VALUE; 
+		int n = graph.length; 	//3
+		int[][] dp=new int[n][graph[0].length];		
+		
+		for(int i=0; i<graph[0].length; i++){
+			dp[0][i] = graph[0][i]; 
+		}
+		
+
+		/*
+		 
+		  	static int[][] graph={
+					{1,2,3,5},
+					
+					{5,6,7,8},
+					{4,3,2,1}
+					};
+		  */
+			for(int i=1; i<n; i++){
+				
+				for(int j=0; j<graph[i].length; j++){
+					
+					int max=Integer.MIN_VALUE; 
+					
+					for(int k=0; k<4; k++){
+						if(j==k)continue; 
+						max = Math.max(max, dp[i-1][k]); 
+					}
+					dp[i][j]=graph[i][j]+max; 
+					
+					
+				}
+				
+			}
+			
+	        for(int i=0;i<4;i++)
+	            answer = Math.max(answer,dp[n-1][i]);
+	        	System.out.println(answer);
+		
+	}
+}
