ae83c65af4ae87575d3404b376d18f33f69616aa
JUNGJUNILL <dala1207@naver.com>
2022-07-08 18:02:46 +0900

    COMMIT

--- src/Practice.java
+++ src/Practice.java
@@ -3,6 +3,16 @@ public class Practice {
 
 	public static void main(String[] args) {
 
-	}
+		
+		String[] c = {"A","B","C","D"};
 
+		
+		for(int i=0; i<c.length; i++){
+			int index = (i+1) % c.length; 
+			System.out.println(index+" : "+c[index]);
+		}
+		
+		
+		
+	}
 }
--- src/leetCode/Recursion/P001.java
+++ src/leetCode/Recursion/P001.java
@@ -1,25 +1,65 @@
 package leetCode.Recursion;
 
+import java.util.Stack;
+
 //Reverse String
 //https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1440/
 public class P001 {
 
-	
+
+		//stack
 	   public static void reverseString(char[] s) {
 	        
-		   int start=0;
-		   int end =s.length-1; 
-		   
-		   for(int i=0; i<s.length; i++){
-			   
-		   }
-		   
-		  char[] newChar1= {'h','e','l','l','o'};
-		  char[] newChar2= {'o','l','l','e','h'};
+		  Stack<Character> stack=new Stack<>();
+		  
+		  for(char c : s){
+			  stack.push(c); 
+		  }
+		  
+		  for(int i=0; i<s.length; i++){
+			  s[i] = stack.pop(); 
+		  }
+		  
+		  	for(char aa : s){
+		  		System.out.println(aa);
+		  	}
 		  
 	    }
+	   
+	   //투포인터
+	   public static void func001(char[] s){
+		   
+		   int start=0;
+		   int end = s.length-1; 
+		   		   
+		   while(start<end){
+			   char first = s[start];
+			   s[start] = s[end]; 
+			   s[end] = first; 
+			   
+			   start++;
+			   end--;
+		   }
+		   
+		  	for(char aa : s){
+		  		System.out.println(aa);
+		  	}
+		   
+		   
+	   }
+	   
+	   
+	   
 	public static void main(String[] args) {
 
-	}
+		  char[] newChar1= {'1','2','3','4'};
+		  //D B C D
+		  char[] newChar2= {'D','C','B','A'};
+	
+		  
+		  reverseString(newChar2); 
+		  System.out.println("---------------");
+		  func001(newChar1); 
+	}	
 
 }
